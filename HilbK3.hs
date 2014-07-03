2efbdc7a762bac89c7f9c8fde3b91462ab9ecb2e
NicolasMalebranche <nicomalebranche@gmail.com>
2014-07-03 18:22:34 +0200

    Erste Version des Tripelprodukts hingeschrieben

--- HilbK3.hs
+++ HilbK3.hs
@@ -9,7 +9,7 @@ import LinearAlgebra
 import Permutation
 import Partitions
 import Data.Permute
-
+import qualified Data.Set as Set
 
 -- Lehn Sorger Defekt
 defect pi tau = div (size pi + 2 - length (cycles pi) - length (cycles tau) 
@@ -23,6 +23,7 @@ defect pi tau = div (size pi + 2 - length (cycles pi) - length (cycles tau)
 --enlarge v (l,a) = v (sortBy (flip compare) (map length l), a)
 
 -- In Lehn und Sorger f^(pi,<pi,tau>)
---fTop pi tau v 
+-- eigentlich nicht wohldefiniert, wegen Relevanz der Anordnung
+-- das ist aber egal, falls mit symmetrisierten Vektoren gerechnet wird
 
-cup g f (part, c) = su
+tripelprodukt zw def (f1,f2) = product [ {-graphdefect-}cupL a ([ ai| (bi,ai) <- f1, Set.isSubsetOf bi b] ++[ai| (bi,ai) <- f2, Set.isSubsetOf bi b])| (b,a) <- zw]
