485e99faf1261e6d870b095c47d1114d2701d8b9
NicolasMalebranche <nicomalebranche@gmail.com>
2014-07-16 10:54:41 +0200

    Cup Produkt Symmetrisierung korrigiert

--- HilbK3.hs
+++ HilbK3.hs
@@ -26,14 +26,23 @@ cupSA :: (PartitionLambda Int, [Int]) -> (PartitionLambda Int, [Int]) -> (Partit
 cupSA (pc,lc) (pa,la) (pb,lb) = sum [res pi | pi <- partAllPerms pa] where
 	pitau = partPermute pc
 	sortedOrbits pi = Data.List.sortBy (flip compare) $ map Set.fromList $ cycles pi
+	permuteGrp (PartLambda l, a) = map concat $ symmetrize $
+		map (snd . unzip) $ groupBy (\a b -> fst a==fst b) $ zip l a
+	symmetrize [] = [[]]
+	symmetrize (l:r) = [pl : a | a <- symmetrize r, pl <- nub $ permutations l]
+	-- Möglichkeiten, die Klassen den Orbits zuzuordnen
+	pga = permuteGrp (pa,la)
+	pgb = permuteGrp (pb,lb)
 	res pi = if cyctau == pb then cupSc else 0 where 
 		tau = compose (inverse pi) pitau
 		cyctau = PartLambda $ Data.List.sortBy (flip compare) $ map length $ cycles tau
 		cmn = map Set.fromList $ commonOrbits pi tau
 		cl = [(or,i) | or <- sortedOrbits pitau | i<-lc]
-		bl = [(or,i) | or <- sortedOrbits tau | i<-lb]
-		al = [(or,i) | or <- sortedOrbits pi | i<-la]
-		cupSc = cupSym cl cmn al bl
+		bls r = [(or,i) | or <- sortedOrbits tau | i<-r]
+		als r = [(or,i) | or <- sortedOrbits pi | i<-r]
+		-- Es reicht, über die Möglichkeiten für die Eingabe zu sortieren. 
+		cupSc = sum [ cupSym cl cmn (als pla) (bls plb) | pla<-pga, plb<-pgb]
+
 
 -- Multiplikation in Lehn & Sorgers A{S_n}
 -- Ausgabe -> Gemeinsame Orbits -> Eingabe1 -> Eingabe2 -> Faktor
