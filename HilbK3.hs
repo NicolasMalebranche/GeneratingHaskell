98ef6d81d9ce02ac8bf3e532230472eb85d98f1b
NicolasMalebranche <nicomalebranche@gmail.com>
2014-09-09 13:58:22 +0200

    Integertrafos jetzt hoffentlich berichtigt

--- HilbK3.hs
+++ HilbK3.hs
@@ -49,7 +49,14 @@ cupSA (pc,lc) (pa,la) (pb,lb) = sum [res pi | pi <- partAllPerms pa] where
 		-- Es reicht, über die Möglichkeiten für die Eingabe zu summieren. 
 		cupSc = sum [ cupSym cl cmn (als pla) (bls plb) | pla<-pga, plb<-pgb]
 
-
+-- Ergibt Liste mit Nicht-Null Elementen
+cupSAsp n deg = cl where
+	base = hilbBase n deg
+	cl ((pa,la),(pb,lb)) = [((pc,lc),x) | (pc,lc) <- base, check pc pa pb lc la lb, let x = cupSA (pc,lc) (pa,la) (pb,lb), x/=0]
+	check (PartLambda c) (PartLambda a) (PartLambda b) lc la lb = u && v  where
+		ac = all(==1) c; aa = all(==1) a; ab=all(==1) b
+		u = if ac then a==b else if ab then a==c  else if aa then b==c else True
+		v = if ab && aa && all (/=23) lc then take n (Data.List.sortBy(flip compare)(la++lb))==lc else True
 
 -- Multiplikation in Lehn & Sorgers A{S_n}
 -- Ausgabe -> Gemeinsame Orbits -> Eingabe1 -> Eingabe2 -> Faktor
@@ -64,18 +71,16 @@ cupSym cList commonOrbits aList bList = product [ sum (x o) | o <- commonOrbits 
 		defekt = div (Set.size o + 2 - length xa - length xb - length xc) 2
 
 -- Ganzzahlige Basis nach Qin / Wang
--- integerBase = integerCreation * creation
 -- Achtung: Koffizienten sind nur rational!
 integerCreation (pi,li) (pc,lc) = if del==0 then 0 else prod / fromIntegral (partZ pc0) where
-	prod = product [powerMonomial (subpart (pc,lc) a) (subpart (pi,li) a) | a<-[1..22]]
+	prod = product [powerMonomial (subpart (pi,li) a) (subpart (pc,lc) a) | a<-[1..22]]
 	(pc0,pi0) = (subpart (pc,lc) 0 , subpart (pi,li) 0)
 	(pcz,piz) = (subpart (pc,lc) 23, subpart (pi,li)23)
 	del = delta pc0 pi0 * delta pcz piz
 
 -- Ganzzahlige Basis nach Qin / Wang
--- creation = creationInteger * integerBase
 creationInteger (pc,lc) (pi,li) = if del==0 then 0 else fromIntegral $ prod * partZ pc0 where
-	prod = product [monomialPower (subpart (pi,li) a) (subpart (pc,lc) a)  | a<-[1..22]]
+	prod = product [monomialPower (subpart (pc,lc) a) (subpart (pi,li) a) | a<-[1..22]]
 	(pc0,pi0) = (subpart (pc,lc) 0 , subpart (pi,li) 0)
 	(pcz,piz) = (subpart (pc,lc) 23, subpart (pi,li)23)
 	del = delta pc0 pi0 * delta pcz piz
@@ -95,28 +100,6 @@ hilbOperators = memo2 hb where
 		[(nn,a):x | nn<-[1..n::Int], a <-[1..22], x<-hilbOperators(n-nn)(d-2*nn)] ++
 		[(nn,23):x | nn <-[1..n], x<-hilbOperators(n-nn)(d-2*nn-2)] 
 
--- Baut die Indizes des symmetrischen Produkts
-sym2 vs = concat [map (\v2-> (v1,v2)) (drop i vs) | v1<-vs| i<-[0..]]
-
-cupIntegral (pc,lc) (pa,la) (pb,lb) = toInt res where
-	doubBase = [(x,y) | x<-filter (\a->integerCreation a (pa,la)/=0) ( nonZero(pa,la)) , 
-		y <- filter (\a->integerCreation a (pb,lb)/=0)(nonZero(pb,lb))  ]
-	singBase = filter (\a->creationInteger (pc,lc) a/=0) $ nonZero(pc,lc)
-	ch c (x,y) = cupSA c x y
-	ci (x,y) (a,b) = integerCreation x a * integerCreation y b 
-	res = sum[creationInteger (pc,lc) sb * ch sb db * ci db ((pa,la),(pb,lb)) | sb <- singBase, db <- doubBase]
-	nonZero (px,lx) =  co where
-		co = map ((\(a,b)->(PartLambda a,b)).unzip.Data.List.sortBy (flip compare).concat) $ combinations $ map allIn [0..23]
-		allIn a = [ [(i,a)|i<-l]| PartLambda l<-map partAsLambda $ partOfWeight $ partWeight $ subpart (px,lx) a ]
-
--- Ergibt Liste mit Nicht-Null Elementen
-cupSAsp n deg = cl where
-	base = hilbBase n deg
-	cl ((pa,la),(pb,lb)) = [((pc,lc),x) | (pc,lc) <- base, check pc pa pb lc la lb, let x = cupSA (pc,lc) (pa,la) (pb,lb), x/=0]
-	check (PartLambda c) (PartLambda a) (PartLambda b) lc la lb = u && v  where
-		ac = all(==1) c; aa = all(==1) a; ab=all(==1) b
-		u = if ac then a==b else if ab then a==c  else if aa then b==c else True
-		v = if ab && aa && all (/=23) lc then take n (Data.List.sortBy(flip compare)(la++lb))==lc else True
 
 -- Hilfsfunktion: Filtert Erzeugerkompositionen
 subpart (PartLambda pl,l) a = PartLambda $ sb pl l where
@@ -125,14 +108,19 @@ subpart (PartLambda pl,l) a = PartLambda $ sb pl l where
 	sb (e:pl) (la:l) = if la == a then e: sb pl l else sb pl l
 
 -- Schreibt Multiplikationsmatrix für Produkte mit 2 Faktoren vom Grad 2
-writeSym2 n = writeFile ("GAP_Code/GAP_n="++show n++"_Sym2.txt") $ showGapMat h4 sh2 m where
-	m i (j1,j2) = cupIntegral i j1 j2
-	h4 = reverse $ hilbBase n 4 
-	sh2 = sym2$hilbBase n 2
+writeSym2 n = writeFile ("GAP_Code/GAP_n="++show n++"_Sym2.txt") m where
+	m = "a:= [\n" ++ concat (intersperse",\n"$ map (show.col) h22) ++"\n];;\n"
+	ic = memo2 integerCreation
+	h4 = hilbBase n 4 
+	h2 = hilbBase n 2;  h22 = [(i,j) | i<-h2, j<-h2, i<=j]
+	cup = cupSAsp n 4
+	base2 (i,j) = [((r,s),u*v) | r<-h2, let u=ic r i, u/=0, s<-h2, let v=ic s j, v/=0]
+	cup2 ij = sumSparse [multSparse uv $ cup rs | (rs,uv) <- base2 ij ]
+	col ij = [ toInt$sum[creationInteger x4 y4 * z | (x4,z) <- cup2 ij] | y4<-h4 ]
 
 -- Schreibt Multiplikationsmatrix für Produkte mit Faktoren von Grad 2 und 4
 -- dro und tak geben Zeilenbereiche an (zum Aufteilen auf meherere Prozesse)
-write24 n = writeFile ("GAP_Code/GAP_n="++show n++"_24'.txt")  m where
+write24 n = writeFile ("GAP_Code/GAP_n="++show n++"_24.txt")  m where
 	m = "a:= [\n" ++ concat (intersperse",\n"[show$col y4 y2 |y4<-h4,y2<-h2] ) ++"\n];;\n"
 	h2 = hilbBase n 2
 	h4 = hilbBase n 4 
@@ -143,9 +131,9 @@ write24 n = writeFile ("GAP_Code/GAP_n="++show n++"_24'.txt")  m where
 	base2 y2 y4 = [((x2,x4),u*v) | (x4,u) <- ic4 y4,  x2<-h2, let v=ic x2 y2, v/=0]
 	cup2 y2 y4 = sumSparse [mult uv $ cup x24 | (x24,uv) <- base2 y2 y4]  
 	mult alpha = map (\(a,v)->(a,alpha*v))
-	col y2 y4 = [z| y6<-h6,let z = toInt$sum [z*creationInteger y6 x6 | (x6,z) <- cup2 y2 y4] ]
+	col y2 y4 = [z| y6<-h6,let z = toInt$sum [z*creationInteger x6 y6 | (x6,z) <- cup2 y2 y4] ]
 
-
+multSparse alpha = map (\(a,v)->(a,alpha*v))
 sumSparse = foldr somme [] where
 	somme a [] = a
 	somme [] b = b
@@ -153,10 +141,9 @@ sumSparse = foldr somme [] where
 		| aa < bb = (aa,va):somme ra b
 		| otherwise=(bb,vb):somme a rb
 
-
 toInt q = if n ==1 then z else if (z `div` n)*n==z then (z`div`n) else 7777777 where (z,n) =(numerator q, denominator q)
 
-writeSym3 n = writeFile ("GAP_Code/GAP_n="++show n++"_Sym3'.txt") s where
+writeSym3 n = writeFile ("GAP_Code/GAP_n="++show n++"_Sym3.txt") s where
 	h2 = hilbBase n 2; h4 = hilbBase n 4; h6 = hilbBase n 6
 	h222 = [(i,j,k)| i<-h2, j<-h2, i<=j, k<-h2, j<=k]
 	mult alpha = map (\(a,v)->(a,alpha*v))
@@ -166,5 +153,5 @@ writeSym3 n = writeFile ("GAP_Code/GAP_n="++show n++"_Sym3'.txt") s where
 	ic = memo2 integerCreation
 	base3 (i,j,k) = [((n,m,l),x*y*z)|n<-h2,let x=ic n i,x/=0, m<-h2,let y=ic m j, y/=0, l<-h2, let z=ic l k, z/=0]
 	icup3 ijk = sumSparse [mult xyz $ cup3 nml | (nml,xyz)<-base3 ijk ]
-	line (i,j,k) = [toInt$sum [v*creationInteger r q|(q,v)<-icup3(i,j,k)]| r<-h6]
+	line (i,j,k) = [toInt$sum [v*creationInteger q r|(q,v)<-icup3(i,j,k)]| r<-h6]
 	s = "a := [\n" ++ concat(intersperse ",\n" [show $line ijk | ijk<-h222]) ++"\n];;\n"
