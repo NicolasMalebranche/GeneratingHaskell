b0a7d26e7c06ef4f4c64980fb9d13e47c8f31185
NicolasMalebranche <nicomalebranche@gmail.com>
2014-09-17 22:55:22 +0200

    Generierte Dateien bereinigt

--- Main.hs
+++ Main.hs
@@ -1,5 +1,19 @@
 import HilbK3
 
 main = do 
-	writeSym3 4
-	writeSym3 5
+	write24 2
+	write24 3
+	write24 4
+	write24 5
+	write24 6
+	write24 7 
+	write22 2
+	write22 3
+	write22 4
+	write22 5
+	write222 2
+	write222 3
+	write222 4
+	write222 5
+	write222 6
+	write222 7
