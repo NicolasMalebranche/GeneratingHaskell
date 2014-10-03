56ba1330a6ee206a18fccd54576cc66e5907bffb
NicolasMalebranche <nicomalebranche@gmail.com>
2014-10-03 18:48:02 +0200

    Signaturalgorithmus geschrieben

--- MatrixAlgorithms.hs
+++ MatrixAlgorithms.hs
@@ -43,3 +43,25 @@ invertUpperDiag1 vs a = iud where
 	inv i k | i>k = 0
 		| i==k = 1
 		| otherwise = - sum [iud i j * a j k | j<-vs, j<k]
+
+-- Signatur einer symmetrischen Matrix: (positive, null, negative) Eigenwerte
+-- Der Koeffizientenbereich muss ein Koerper sein, um gigantische Zahlen zu vermeiden 
+-- www.cas.mcmaster.ca/~cs777/presentations/voicu.pdf 
+signature vs m = if el1/=0 then alg1 v1 else 
+	if el2/=0 then alg2 v2 else (0,fromIntegral $ length vs,0) where 
+	(el1,v1) = piv1 [] vs; (el2,v2) = piv2 [] vs 
+	piv1 acc [] = (0, acc)
+	piv1 acc (i:r) = if el /= 0 then (el, i:acc++r) else piv1 (i:acc) r where el = m i i
+	alg1 (i:r) = if el1 < 0 then (a,b,c+1) else (a+1,b,c) where
+		(a,b,c) = signature r mm 
+		mi = memo (m i)
+		mm p q = m p q - mi p * mi q / el1
+	piv2 acc [] = (0,acc)
+	piv2 acc (i:r) = p [] r where
+		p a2 [] = piv2 (i:acc) r
+		p a2 (j:s) = if el/=0 then (el,i:j:acc++a2++s) else p (j:a2) s where el = m j i
+	alg2 (i:j:r) = (a+1,b,c+1) where
+		(a,b,c) = signature r mm
+		mi = memo (m i); mj = memo (m j)
+		mm p q =  m p q - (mi p * mj q + mi q * mj p) / el2
+
