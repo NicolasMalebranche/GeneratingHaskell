48f169fbd74931bb30eac9683ca22b56a7cb6e9d
NicolasMalebranche <nicomalebranche@gmail.com>
2014-10-10 18:58:18 +0200

    Signatur weitergewurschtelt

--- Signature.hs
+++ Signature.hs
@@ -1,4 +1,4 @@
-module Signature(signatur) where
+module Signature where
 
 import Data.MemoTrie
 import LinearAlgebra
@@ -25,6 +25,7 @@ mapWriteArray f ar = mw where
 
 gg _ = undefined :: IO (IOArray a b)
 
+{-
 signatur vs m = unsafePerformIO sig where
 	mvs = map m vs
 	-- Typ von m noch korrigieren!!
@@ -32,7 +33,7 @@ signatur vs m = unsafePerformIO sig where
 	sparseLook i im = g $ IM.lookup i im where g Nothing = 0; g (Just x) = x
 	inc x (a,b,c) = if x>0 then (a+1,b,c) else if x<0 then (a,b,c+1) else (a,b+1,c)
 	sig = do 
-		ar <- if False then gg 1 else newListArray (1,length vs) mvs
+		ar <- if False then gg 1 else newListArray (1,length vs) $ map IM.fromList mvs
 		algo ar (0,0,0) (minView pqstart)
 	algo ar = iter where
 		iter acc Nothing = return acc
@@ -44,9 +45,27 @@ signatur vs m = unsafePerformIO sig where
 			mapWriteArray update ar $ IM.keys kline
 			let pqu = undefined
 			iter (inc diag acc) $ minView pqu
+-}
 
+signature vs m = unsafePerformIO run where
+	mvs = map m vs
+	pqInit = fromList $ zipWith ( :-> ) [1..] $ map length mvs
+	run = do
+		ar <- if False then gg 1 else newListArray (1,length vs) $ map IM.fromList mvs
+		iter ar pqInit (0,0,0)
+	iter ar pq sig = if Data.PSQueue.null pq then return sig else do
+		(ps,npq) <- pivotStrategy ar pq
+		nnpq <- applyPivot ar npq ps
+		iter ar nnpq (incPivot sig ps)
 
-data Pivot a = Pi Int a | Bi Int Int a 
+data Pivot a = Pi Int a | Bi Int Int a | Tri Int Int a a | Ignore
+
+pivotStrategy ar pq = undefined where
+	sparseLook i im = g $ IM.lookup i im where g Nothing = 0; g (Just x) = x
+	createStrat (Just (k :-> pr, pq)) = do
+		kline <- readArray ar k
+		let diag = sparseLook k kline
+		if diag/=0 then return $ Pi k diag else undefined
 
 applyPivot ar pq = app where
 	subtract _ pqu [] = return pqu
@@ -67,28 +86,20 @@ applyPivot ar pq = app where
 		let line p = IM.unionWith (+) (IM.map (*(jline IM.!p/ndia)) iline) 
 			(IM.map (*(iline IM.!p/ndia)) jline)
 		subtract line pq $ Asc.union (IM.keys iline) (IM.keys jline) 
+	app (Tri i j diag ndia) = do
+		iline <- readArray ar i
+		jline <- readArray ar j
+		let line p = IM.unionWith (+) (IM.map (*(jline IM.!p/ndia)) iline) $
+			IM.unionWith (-) (IM.map (*(iline IM.!p/ndia)) jline)
+				(IM.map (*(iline IM.!p*diag/ndia^2)) iline) 
+		subtract line pq $ Asc.union (IM.keys iline) (IM.keys jline) 
+	app Ignore = return pq
 
+incPivot (a,b,c) = i where
+	i (Pi _ v) = if v<0 then (a,b,c+1) else (a+1,b,c)
+	i (Bi _ _ _) = (a+1,b,c+1)
+	i (Tri _ _ _ _) = (a+1,b,c+1)
+	i Ignore = (a,b+1,c)
 	
 
-
--- Signatur einer symmetrischen Matrix: (positive, null, negative) Eigenwerte
--- Der Koeffizientenbereich muss ein Koerper sein, um gigantische Zahlen zu vermeiden 
--- www.cas.mcmaster.ca/~cs777/presentations/voicu.pdf 
-signature vs m = if el1/=0 then alg1 v1 else 
-	if el2/=0 then alg2 v2 else (0,fromIntegral $ length vs,0) where 
-	(el1,v1) = piv1 [] vs; (el2,v2) = piv2 [] vs 
-	piv1 acc [] = (0, acc)
-	piv1 acc (i:r) = if el /= 0 then (el, i:acc++r) else piv1 (i:acc) r where el = m i i
-	alg1 (i:r) = if el1 < 0 then (a,b,c+1) else (a+1,b,c) where
-		(a,b,c) = signature r mm 
-		mi = memo (m i)
-		mm p q = m p q - mi p * mi q / el1
-	piv2 acc [] = (0,acc)
-	piv2 acc (i:r) = p [] r where
-		p a2 [] = piv2 (i:acc) r
-		p a2 (j:s) = if el/=0 then (el,i:j:acc++a2++s) else p (j:a2) s where el = m j i
-	alg2 (i:j:r) = (a+1,b,c+1) where
-		(a,b,c) = signature r mm
-		mi = memo (m i); mj = memo (m j)
-		mm p q =  m p q - (mi p * mj q + mi q * mj p) / el2
 
