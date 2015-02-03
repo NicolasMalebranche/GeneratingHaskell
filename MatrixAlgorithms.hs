0fce32ad950051d9fc302cbd0e6ca60f16a56851
NicolasMalebranche <nicomalebranche@gmail.com>
2015-02-03 11:58:51 +0100

    Berkowitz-Algorithmus angepasst

--- MatrixAlgorithms.hs
+++ MatrixAlgorithms.hs
@@ -2,27 +2,27 @@
 module MatrixAlgorithms where
 
 import Data.MemoTrie
+import Data.Array
 import LinearAlgebra
 
--- Hilfsfunktinoen für Lineare Algebra
+-- Hilfsfunktionen für Lineare Algebra
 
 -- Berechnet die Koeffizienten des charakteristischen
 -- Polynoms mit dem Algorithmus von Berkowitz.
 -- Die Determinante ist das letzte Listenelement.
 -- vs sind die Indizes der quadratischen Matrix m
 berkowitz vs m = let 
-	c j = memo2 toep where
+	c j = toep where
 		ii = vs!!(j-1)
 		toep i k | k>i = 0
 			| k==i = 1
-			| otherwise = - rms !! (i-k-1)
-		rms = [m ii ii | m <- mpowers]
-		mpowers = m : map (mM (take (j-1) vs) m) mpowers
-	p 0 = delta
-	p j = mM [0..j+1] (c j) (p (j-1)) 
+			| otherwise = rms ! (i-k-1)
+		rms = listArray (0,n) [-vElem mr ii | mr <- mpowers]
+		mpowers = scanl (vM $take (j-1) vs) (mRow m ii) (repeat m)
+	p 0 = delta 0
+	p j = mV [0..j+1] (c j) (p (j-1)) 
 	n = length vs
-	pn = p n
-	in [pn i 0 | i <-[0..n]]
+	in map (p n) [0..n]
 
 -- Berechnet die adjunkte Matrix und die Determinante mit
 -- dem Berkowitz-Algorithmus. Die Inverse ist Adjunkte/Det.
