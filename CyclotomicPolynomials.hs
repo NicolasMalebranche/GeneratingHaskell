b07bc1e252d2fce7f7c9d9573212f86d49885848
NicolasMalebranche <nicomalebranche@gmail.com>
2015-12-26 12:31:31 +0100

    Zyklotomische Polynome programmiert

---
+++ CyclotomicPolynomials.hs
@@ -0,0 +1,12 @@
+
+module CyclotomicPolynomials where
+
+import Polynomial
+import PowerSeries
+import PrimeFactors
+
+cyclotomic n = run 1 1 (seriesGenerating [1,-1]) pf where
+	pf = primeFactors n
+	run deg s c [] = polyFromSeries (s*deg) $ seriesStretch s c
+	run deg s c ((a,m):r) = run (deg*(a-1)) (s*a^(m-1)) newc r where
+		newc = seriesStretch a c * seriesInv c
