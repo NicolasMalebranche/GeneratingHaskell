11e3b60ea21eaa60579dc908e9eb6d01c6748a29
NicolasMalebranche <nicomalebranche@gmail.com>
2016-02-11 16:01:06 +0100

    Bessere Primfaktorzerlegung

--- PrimeFactors.hs
+++ PrimeFactors.hs
@@ -4,11 +4,12 @@ import Data.Numbers.Primes(primes)
 
 -- Primfaktorzerlegung durch Ausprobieren.
 -- Ergebnis ist Liste aus (Primzahl, Vielfachheit)
-primeFactors  n = if n==0 then [] else trial primes $ abs n where
-	trial (p:r) n = if n==1 then [] else if m==0 then up [p] d else trial r n where 
-		(d,m) = divMod n p
-		up ps n = if m==0 then up (p2:ps) d else down ps 1 n where p2 = head ps^2; (d,m) = divMod n p2
-		down [] mu n = (p,mu-1) : trial r n
-		down (p:r) mu n = if m==0 then down r (2*mu+1) d else down r (2*mu) n where (d,m) = divMod n p
-
-
+primeFactors n = if n==0 then [] else trial (bound n) primes $ abs n where
+	bound n = ceiling $ sqrt $ (fromIntegral n :: Double)
+	trial b (p:r) n = if p>b then if n/=1 then [(n,1)] else [] else 
+		case divide n p 0 of
+		(_,0) -> trial b r n
+		(nn,m) -> (p,m) : trial (bound nn) r nn 
+	divide n s mult = case divMod n s of
+		(d,0) -> divide d s (mult+1)
+		_ -> (n,mult) 
