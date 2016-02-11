6940ddd5e601998c294502298a24aaab1fe8be61
NicolasMalebranche <nicomalebranche@gmail.com>
2016-02-11 15:01:28 +0100

    Enum-Instanz, Verkürztes Show

--- F_p.hs
+++ F_p.hs
@@ -16,6 +16,7 @@ instance ModP Mod2 Int where
 	p _ = 2
 	con = Mod2
 	decon (Mod2 i) = i
+	primRoot = 1
 
 data Mod3
 newtype instance F Mod3 = Mod3 Int
@@ -23,6 +24,7 @@ instance ModP Mod3 Int where
 	p _ = 3
 	con = Mod3
 	decon (Mod3 i) = i
+	primRoot = 2
 
 data Mod5
 newtype instance F Mod5 = Mod5 Int
@@ -30,6 +32,7 @@ instance ModP Mod5 Int where
 	p _ = 5
 	con = Mod5
 	decon (Mod5 i) = i
+	primRoot = 2
 
 data Mod7
 newtype instance F Mod7 = Mod7 Int
@@ -37,6 +40,7 @@ instance ModP Mod7 Int where
 	p _ = 7
 	con = Mod7
 	decon (Mod7 i) = i
+	primRoot = 3
 
 -- Fuer p > 2^16 sollte man seine Zahlen nicht
 -- mehr als Int speichern, sondern z. B. als Integer
@@ -56,9 +60,10 @@ class Integral i => ModP a i | a->i where
 	p :: F a -> i
 	con :: i -> F a
 	decon :: F a -> i
+	primRoot :: F a
 
 instance (ModP fp i, Show i) => Show (F fp) where
-	show x = show (decon x) ++ " (mod " ++ show (p x) ++ ")"
+	show x = show (decon x) -- ++ " (mod " ++ show (p x) ++ ")"
 
 instance (ModP fp i) => Eq (F fp) where
 	a == b = 0 == ((decon a - decon b) `mod` (p a))
@@ -81,6 +86,16 @@ instance (ModP fp i) => Fractional (F fp) where
 			(gcd, (x, y)) = euclid m a
 	fromRational r = fromInteger (numerator r) / 
 		fromInteger (denominator r)
+
+instance (ModP fp i) => Enum (F fp) where
+	toEnum i = x where x = con $ fromIntegral i `mod` p x
+	fromEnum = fromIntegral . decon
+	succ x = x+1
+	pred x = x-1
+	enumFromTo a b = if a==b then [a] else a : enumFromTo (a+1) b
+	enumFrom a = enumFromTo a (a-1)
+	enumFromThenTo a s b = if a == b then [a] else a : enumFromThenTo s (s*2-a) b
+	enumFromThen a s = enumFromThenTo a s (a*2-s)
 
 
 
