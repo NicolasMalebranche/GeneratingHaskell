f3f6e545e2af6c2f52359b360601b54b136cff23
NicolasMalebranche <nicomalebranche@gmail.com>
2016-04-18 13:38:24 +0200

    Neue Routine

--- GroupAction.hs
+++ GroupAction.hs
@@ -29,7 +29,24 @@ gOrbit gen x = run (Set.singleton x) where
 gOrbits gen s = run s where
 	run s = if Set.null s then [] else or : run (s Set.\\ or) where
 		or = gOrbit gen $ head $ Set.elems s
-		
+
+-- Findet kürzesten Weg aus Elementen von gen, um von x nach y zu gelangen		
+gPath gen x y = run (Set.singleton $ Lab x []) where
+	yy = Lab y undefined
+	run s = let 
+		Just (Lab _ path) = Set.lookupLE yy s 
+		ns = Set.unions $ s : [gAct g s | g <- gen]
+		in
+		if Set.member yy s then Just path else 
+		if  s==  ns then Nothing else run ns 
+
+data Labeled a i = Lab a i deriving Show
+instance (Eq a) => Eq (Labeled a i) where
+	Lab a _ == Lab b _  = a==b
+instance (Ord a) => Ord (Labeled a i) where
+	Lab a _ `compare` Lab b _  = compare a b
+instance (GroupAction g a) => GroupAction g (Labeled a [g]) where
+	gAct g (Lab a p) = Lab (gAct g a) (g:p)
 
 -- Ist x ein invariantes Element?
 gInvariant gen x = all (x ==) [ gAct g x | g <- gen ]
