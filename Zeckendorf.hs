e93ec315dc90aee5a19345881aef76314b27d54a
NicolasMalebranche <nicomalebranche@gmail.com>
2016-04-30 12:13:26 +0200

    Auf Negafibonaccis umgestellt

--- Zeckendorf.hs
+++ Zeckendorf.hs
@@ -1,36 +1,41 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module Zeckendorf where
 
 import Data.MemoTrie
 import Data.List
 
-newtype ZeckendorfRepresentation = Zecken [Int]
+-- Darstellung ganzer Zahlen mit Nega-Fibonaccis
+data Zeckendorf = Zecken {zahl::Integer, zeck::[Int]}
 
-instance Show ZeckendorfRepresentation where
-	show (Zecken []) = "0"
-	show (Zecken r) = concat $ intersperse " + " ["f_"++show i | i<-r]
+instance Show Zeckendorf where
+	show = show . zahl
 
+instance Num Zeckendorf where
+	a+b = zecken $ zahl a + zahl b
+	a-b = zecken $ zahl a - zahl b
+	a*b = zecken $ zahl a * zahl b
+	fromInteger = zecken 
+	signum = signum . zecken . zahl 
+	abs = abs . zecken . zahl
+	
 fibonacci = memo f where
 	f :: Int -> Integer	
 	f 0 = 0
 	f 1 = 1
-	f n = fibonacci (n-1) + fibonacci (n-2)
+	f n = if n > 0 
+		then fibonacci (n-1) + fibonacci (n-2)
+		else fibonacci (n+2) - fibonacci (n+1)
 
-goldenRatio = (1+ sqrt5) / 2 :: Double
-sqrt5 = sqrt 5
-logGoldenRatio = log goldenRatio
+goldenRatio = (1+ sqrt 5) / 2 :: Double
 
-zecken 0 = Zecken []
-zecken 1 = Zecken [2]
-zecken 2 = Zecken [3]
-zecken 3 = Zecken [4]
-zecken n = let 
-	m = ceiling $ log (fromIntegral n * sqrt5) / logGoldenRatio
-	is = if fibonacci m > n then (m-1) else m
-	Zecken r = zecken (n-fibonacci is)
-	in Zecken $ is:r
 
-fromZecken (Zecken r) = sum $ map fibonacci r
+zecken = \ n -> Zecken n (z n) where
+	sqrt5 = sqrt 5
+	logGoldenRatio = log goldenRatio
+	z 0 =  []
+	z n = is : z (n-fibonacci is) where
+		m = ceiling $ log (fromIntegral (abs n) * sqrt5) / logGoldenRatio
+		is = if odd m == (n>0) then  - m else 1 - m
+	
+zeckenShift shift a = zecken $ sum $ map (fibonacci .( + shift )) $ zeck a
 
-fibProduct x y = fromZecken $ zeckenProduct (zecken x) (zecken y) where
-	zeckenProduct (Zecken a) (Zecken b) = Zecken [ i+j | i<-a, j<-b]
+fibProduct x y = zecken $ sum [ fibonacci (i+j) | i<-zeck x, j<-zeck y]
