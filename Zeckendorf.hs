8e562b63cd4e3d120d84f80d9e8b838991fbae9a
NicolasMalebranche <nicomalebranche@gmail.com>
2016-05-15 12:31:47 +0200

    Alles neu definiert

--- Zeckendorf.hs
+++ Zeckendorf.hs
@@ -3,19 +3,83 @@ module Zeckendorf where
 import Data.MemoTrie
 import Data.List
 import Data.List.Ordered
-import System.IO.Unsafe
-import Data.IORef
-import PrimeFactors
+
+fibonacci = generalFib 0 1
+	
+lucas = generalFib 2 1
+
+generalFib a b = gen where
+	gen = memo g
+	g :: Int -> Integer
+	g 0 = a 
+	g 1 = b
+	g n = if n > 0 
+		then gen (n-1) + gen (n-2)
+		else gen (n+2) - gen (n+1)
+
+goldenRatio = (1+ sqrt 5) / 2 :: Double
+sqrt5 = sqrt 5
+logGoldenRatio = log goldenRatio
 
 -- Darstellung ganzer Zahlen mit (Nega-)Fibonaccis
-data Zeckendorf = Zecken {zahl::Integer, zeck::[Int]} deriving (Eq,Ord)
+-- Die Liste muß aufsteigend sein
+data Zeckendorf = Zecken {zeck::[Int], zeckNeg:: Bool } deriving (Eq,Ord)
 
+zahl z = (if zeckNeg z then negate else id) $ sum [fibonacci i | i <- zeck z]
+zeckApprox z = (if zeckNeg z then negate else id) $  
+	sum [ goldenRatio ** (fromIntegral i) | i <- zeck z ]
 
-{-# NOINLINE negaRef #-}
-negaRef :: IORef Bool
-negaRef = unsafePerformIO $ newIORef False
-nega _ = unsafePerformIO $ readIORef negaRef
+zeckenShift shift (Zecken a n) = Zecken (map (shift +) a) n	
 
+zecken n = Zecken (reverse $ z $ abs n) (n < 0) where 
+	z 0 =  []
+	z n = is : z (n-fibonacci is) where
+		m = ceiling $ log (fromIntegral (abs n) * sqrt5) / logGoldenRatio
+		is = if fibonacci m > n then (m-1) else m
+
+negafib n = Zecken (z n) False where
+	z 0 =  []
+	z n = is : z (n-fibonacci is) where
+		m = ceiling $ log (fromIntegral (abs n) * sqrt5) / logGoldenRatio
+		is = if odd m == (n>0) then  - m else 1 - m
+
+instance Num Zeckendorf where
+	Zecken a n + Zecken b m  = case (a,b,n,m) of
+		( [], _ , _ , _ ) -> Zecken b m
+		( _ , [], _ , _ ) -> Zecken a n
+		( _ , _ , False, False ) -> plus
+		( _ , _ , False, True ) -> if numa<numb then negate minus else minus
+		( _ , _ , True, False ) -> if numb<numa then negate minus else minus
+		( _ , _ , True, True )  -> negate plus
+		where
+		shift = min (head a) (head b) - 4
+		numa = sum [ fibonacci (i-shift) | i <- a]
+		numb = sum [ fibonacci (i-shift) | i <- b]
+		plus = zeckenShift shift $ zecken $ numa + numb
+		minus = zeckenShift shift$ zecken $ abs $ numa - numb
+	negate (Zecken a n) = Zecken a $ not n
+	fromInteger = zeckMult 
+	abs (Zecken a n) = Zecken a False
+	signum (Zecken a n) = Zecken [0] n
+	Zecken a n * Zecken b m = if (n==m) then mul else -mul where
+		shift = head a + head b - 4
+		mulz = [ fibonacci (i+j-shift) | i<-a, j<-b]
+		mul = if mulz == [] then Zecken [] False 
+			else zeckenShift shift $ zecken $ sum mulz
+		
+-- golden Ratio base
+zeckMult 0 = Zecken [] False
+zeckMult n = Zecken (zeck m) (n<0) where
+	s = ceiling $ log (fromIntegral (abs n) * sqrt5) / logGoldenRatio
+	m = zeckenShift (-s) $ zecken $ abs n * fibonacci s
+	
+{-
+zeckMult n = Zecken (if n == 0 then [] else list) (n<0) where
+	t s = (zahl $ zeckenShift 1 $ zecken $ n*s) `mod` n == 0
+	rep = head $ filter t [1..]
+	[ r ] = zeck $ zecken rep
+	list = map (+ (-r)) $ zeck $ zecken $ rep * n	
+	
 instance Show Zeckendorf where
 	show = show . zahl
 
@@ -41,46 +105,18 @@ instance Integral Zeckendorf where
 	divMod a b = (zecken x,zecken y) where (x,y) = divMod (zahl a) (zahl b)
 	quotRem a b = (zecken x,zecken y) where (x,y) = quotRem (zahl a) (zahl b)
 	
-fibonacci = generalFib 0 1
-	
-lucas = generalFib 2 1
 
-generalFib a b = gen where
-	gen = memo g
-	g :: Int -> Integer
-	g 0 = a 
-	g 1 = b
-	g n = if n > 0 
-		then gen (n-1) + gen (n-2)
-		else gen (n+2) - gen (n+1)
-
-goldenRatio = (1+ sqrt 5) / 2 :: Double
 
 combs = alls :: [[Int]] where
 	alls = inter 0 [[i] | i<-[0..]] [ (head x + n):x | (x,n)<- cantor alls [2..]]
 	cantor (x:a) b = inter 0 [(x,y)|y<-b] $ cantor a b
 	inter n (i:j) r = take n r ++ [i] ++ inter (n+1) j (drop n r)
 
-(zecken, negafib) = (zz False , zz True) where
-	zz nega n = Zecken n (z nega n) 
-	sqrt5 = sqrt 5
-	logGoldenRatio = log goldenRatio
-	z _ 0 =  []
-	z nega n = is : z nega (n-fibonacci is) where
-		m = ceiling $ log (fromIntegral (abs n) * sqrt5) / logGoldenRatio
-		is = if nega 
-			then if odd m == (n>0) then  - m else 1 - m
-			else if fibonacci m > n then (m-1) else m
-	
-zeckenShift shift a = Zecken s m where
-	m = map (shift +) $ zeck a
-	s = sum $ map fibonacci m
 
-zeckMult n = Zecken 0 $ if n == 0 then [] else list where
-	t s = (zahl $ zeckenShift 1 $ zecken $ n*s) `mod` n == 0
-	rep = head $ filter t [1..]
-	[ r ] = zeck $ zecken rep
-	list = map (+ (-r)) $ zeck $ zecken $ rep * n
+	
+
+
+
 
 infixl 8 +*
 
@@ -99,5 +135,5 @@ spezialNRange s = (unten,oben) where
 	oben = run unten
 	run n = if istSpezialMod n s then run (n+1) else n
 	mun n = if istSpezialMod n s then n else mun (n+1)
-	
+-}
 
