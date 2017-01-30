19b4e6b0e00aa02e8a34913eac7fc52abe35d981
Nicolas Malebranche <nicomalebranche@gmail.com>
2017-01-30 08:34:33 +0100

    Compress/inflate refactored

--- CollOps.hs
+++ CollOps.hs
@@ -23,39 +23,37 @@ compops a b  = [a,b]
 
 sortC =  sortBubble compops
 
-data OpNums = T Integer | B Integer deriving (Eq,Ord,Show)
+data OpNums = T Int Integer | B Int Integer deriving (Eq,Ord,Show)
 
 compress = f where
 	f [] = []
-	f (T0:r) = c (T 0) r
-	f (T1:r) = c (T 1) r
-	f (T2:r) = c (T 2) r
-	f (B0:r) = c (B 0) r
-	f (B1:r) = c (B 1) r
+	f (T0:r) = c (T 1 0) r
+	f (T1:r) = c (T 1 1) r
+	f (T2:r) = c (T 1 2) r
+	f (B0:r) = c (B 1 0) r
+	f (B1:r) = c (B 1 1) r
 	c tb [] = [tb]
-	c (T n) (T0:b) = c (T $ 3*n  ) b
-	c (T n) (T1:b) = c (T $ 3*n+1) b
-	c (T n) (T2:b) = c (T $ 3*n+2) b
-	c (T n) (B0:b) = T n: c (B 0) b
-	c (T n) (B1:b) = T n: c (B 1) b
-	c (B n) (T0:b) = B n: c (T 0) b
-	c (B n) (T1:b) = B n: c (T 1) b
-	c (B n) (T2:b) = B n: c (T 2) b
-	c (B n) (B0:b) = c (B $ 2*n  ) b
-	c (B n) (B1:b) = c (B $ 2*n+1) b
+	c (T p n) (T0:b) = c (T (p+1) (3*n))  b
+	c (T p n) (T1:b) = c (T (p+1) (3*n+1)) b
+	c (T p n) (T2:b) = c (T (p+1) (3*n+2)) b
+	c (T p n) (B0:b) = T p n: c (B 1 0) b
+	c (T p n) (B1:b) = T p n: c (B 1 1) b
+	c (B p n) (T0:b) = B p n: c (T 1 0) b
+	c (B p n) (T1:b) = B p n: c (T 1 1) b
+	c (B p n) (T2:b) = B p n: c (T 1 2) b
+	c (B p n) (B0:b) = c (B (p+1) (2*n)) b
+	c (B p n) (B1:b) = c (B (p+1) (2*n+1)) b
 
 
 
 inflate [] = []
-inflate (T 0:r) = T0 : inflate r
-inflate (T n:r) = dec n ++ inflate r where
+inflate (T p n:r) = dec p n ++ inflate r where
 	t 0 = T0; t 1 = T1; t 2 = T2
-	dec 0 = [] 
-	dec n = dec d ++ [t m] where (d,m) = divMod n 3
-inflate (B 0:r) = B0 : inflate r
-inflate (B n:r) = dec n ++ inflate r where
+	dec 0 _ = [] 
+	dec p n = dec (p-1) d ++ [t m] where (d,m) = divMod n 3
+inflate (B p n:r) = dec p n ++ inflate r where
 	b 0 = B0; b 1 = B1
-	dec 0 = [] 
-	dec n = dec d ++ [b m] where (d,m) = divMod n 2
+	dec 0 _ = [] 
+	dec p n = dec (p-1) d ++ [b m] where (d,m) = divMod n 2
 	
 	
